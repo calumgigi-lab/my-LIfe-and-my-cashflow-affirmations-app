99f36c31c714d9da6fc8e969a18c91a3c4738d2f
willsonanderson25-alt <willsonanderson25@gmail.com>
2026-09-02 03:06:38 -0800

    Bump v2.0.4/versionCode 21, .easignore, cleanup, fix text overflow

--- scripts/delete-april-day31.js
+++
@@ -1,5 +0,0 @@
-const postgres = require("postgres");
-const sql = postgres(process.env.DATABASE_URL, { ssl: "require" });
-sql`DELETE FROM affirmations WHERE booklet_id = 297 AND day_number = 31`
-  .then(() => { console.log("Deleted Day 31 from April 2026 booklet"); return sql.end(); })
-  .catch(err => { console.error("Error:", err.message); process.exit(1); });
